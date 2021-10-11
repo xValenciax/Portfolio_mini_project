3a4692854806c25058ff21cc8165d0b497c8b36a
Mahmoud Mohamed <mamo31st@gmail.com>
2021-10-11 08:35:43 +0200

    added some basic tailwindcss classes

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        main: '#212121'
+      },
+      fontFamily: {
+        body: ['Gemunu Libre']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
